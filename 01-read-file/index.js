c4bb0e5f9e12a3b0478f66ef993e1ffcad166b73
julia <s98435@yandex.ru>
2022-05-20 21:37:16 +0300

    fix: change with stream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+const { stdout } = process;
 
-fs.readFile(
-    path.join(__dirname,'text.txt'),
-    'utf-8',
-    (err, data) => {
-        if (err) throw err;
-        console.log(data)
-    }
-)
+// fs.readFile(
+//     path.join(__dirname,'text.txt'),
+//     'utf-8',
+//     (err, data) => {
+//         if (err) throw err;
+//         console.log(data)
+//     }
+// )
+
+const fileData = fs.createReadStream(path.join(__dirname,'text.txt'))
+let data = '';
+fileData.on('data', chunk => data += chunk);
+fileData.on('end', () => stdout.write(`\n ${data}`));
+fileData.on('error', error => stdout.write(`${error}`))
