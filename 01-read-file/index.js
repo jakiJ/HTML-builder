8ed38b6a9da6c68d6918847da2cbea5e57b4ff39
julia <s98435@yandex.ru>
2022-05-20 14:00:19 +0300

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(
+    path.join(__dirname, ' 01-read-file','text.txt'),
+    'utf-8',
+    (err, data) => {
+        if (err) throw err;
+        console.log(data)
+    }
+)
