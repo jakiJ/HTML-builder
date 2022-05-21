d8b6b08a2f2109826d0fd8d6e5f1c094cff4f538
julia <s98435@yandex.ru>
2022-05-21 19:04:17 +0300

    feat: add solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
 
+const pathToDir = path.join(__dirname, 'files');
+
+fs.mkdir(`${__dirname}/files-copy`,{recursive: true}, (error) => {
+    if (error) throw error;
+  });
+
+function copyDir(dirPath) {
+    fs.readdir(dirPath, (error, items) => {
+        if (error) throw error;
+        items.forEach(item => {
+            const readFile = fs.createReadStream(`${dirPath}/${item}`)
+            const writeFile = fs.createWriteStream(`${dirPath}-copy/${item}`)
+            readFile.on('data', data => {
+                writeFile.write(`${data.toString()}`)
+            })
+        })
+    })
+    stdout.write(`Copy directory create or refresh`)
+}
+
+copyDir(pathToDir)
+
+// fs.watch(pathToDir, (event) => {
+//     if (event == 'change') {
+//         copyDir(pathToDir)
+//     }
+// })
