aacc2bc15267a632c03858c99bb872a3e29f3f4a
julia <s98435@yandex.ru>
2022-05-21 20:08:13 +0300

    feat: add solve 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const pathToDir = path.join(__dirname, 'styles');
+const writeFile = fs.createWriteStream(`${__dirname}/project-dist/bundle.css`)
+
+fs.mkdir(`${__dirname}/project-dist`,{recursive: true}, (error) => {
+    if (error) console.log(error);
+  });
+
+function styleBundler(pathDir) {
+    fs.readdir(pathDir, (error, items) => {
+        if (error) console.log(error);
+        items.forEach(item => {
+            if (path.extname(`${pathDir}/${item}`) == '.css') {
+                const readFile = fs.createReadStream(`${pathDir}/${item}`)
+                readFile.on('data', data => {
+                    writeFile.write(data)
+                })
+            }
+        })
+    })
+}
+
+styleBundler(pathToDir)
