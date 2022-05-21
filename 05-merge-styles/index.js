0cf2f758e6ea075867f24510ce34d565ae02b5fa
julia <s98435@yandex.ru>
2022-05-21 20:11:58 +0300

    refactor: delete not use modules

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,17 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const { stdout } = process;
 
 const pathToDir = path.join(__dirname, 'styles');
 const writeFile = fs.createWriteStream(`${__dirname}/project-dist/bundle.css`)
 
 fs.mkdir(`${__dirname}/project-dist`,{recursive: true}, (error) => {
-    if (error) console.log(error);
+    if (error) throw error;
   });
 
 function styleBundler(pathDir) {
     fs.readdir(pathDir, (error, items) => {
-        if (error) console.log(error);
+        if (error) throw error;
         items.forEach(item => {
             if (path.extname(`${pathDir}/${item}`) == '.css') {
                 const readFile = fs.createReadStream(`${pathDir}/${item}`)
