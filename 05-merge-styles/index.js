f0e6b4af261383c43572ce57262b10a6b779eef7
julia <s98435@yandex.ru>
2022-05-23 23:02:39 +0300

    feat: add remove bundel before refresh

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
+const fsPromises = fs.promises;
 const path = require('path');
 
 const pathToDir = path.join(__dirname, 'styles');
-const writeFile = fs.createWriteStream(`${__dirname}/project-dist/bundle.css`)
+// const writeFile = fs.createWriteStream(`${__dirname}/project-dist/bundle.css`)
 
-fs.mkdir(`${__dirname}/project-dist`,{recursive: true}, (error) => {
-    if (error) throw error;
-  });
+// fs.mkdir(`${__dirname}/project-dist`,{recursive: true}, (error) => {
+//     if (error) throw error;
+//   });
 
-function styleBundler(pathDir) {
+async function styleBundler(pathDir) {
+    await fsPromises.rm(`${__dirname}/project-dist/bundle.css`, { recursive: true, force: true })
+    const writeFile = fs.createWriteStream(`${__dirname}/project-dist/bundle.css`)
     fs.readdir(pathDir, (error, items) => {
         if (error) throw error;
         items.forEach(item => {
